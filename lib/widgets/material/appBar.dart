68249b2bcb7346ead4677c254066cab3fa88d068
hyungeun.jin <kingwang09@gmail.com>
2023-04-13 17:27:48 +0900

    Widgets 탐방: appBar 한 클래스에 넣으니 반응이 없네..? -> 별도 클래스로 Home을 분리하니 정상 동작함.

--- lib/widgets/material/appBar.dart
+++ lib/widgets/material/appBar.dart
@@ -10,50 +10,62 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(MyAppBar());
+  runApp(const MyAppBar());
 }
 
 class MyAppBar extends StatelessWidget{
+  const MyAppBar({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Appbar Demo...'),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.add_alarm),
-                onPressed: (){
-                  ScaffoldMessenger
-                      .of(context)
-                      .showSnackBar(const SnackBar(content: Text('This is a snackbar')));
-                },
-            ),
-            IconButton(
-                icon: Icon(Icons.navigate_next),
-                onPressed: (){
-                  Navigator.push(context,
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context){
-                          return Scaffold(
-                            appBar: AppBar(
-                              title: Text("next Page: 다음 페이집니다."),
-                            ),
-                            body: Center(
-                              child: Text("두번째 페이집니다...."),
-                            ),
-                          );
-                        }
-                      )
-                  );
-                },
-            )
-          ],
-        ),
-        body: Center(
-          child: Text(
-            "이것이 첫 페이지 입니다..."
+    return const MaterialApp(
+      home: AppBarHome()
+    );
+  }
+
+}
+
+class AppBarHome extends StatelessWidget {
+  const AppBarHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar Demo...'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_alarm),
+            onPressed: (){
+              ScaffoldMessenger
+                  .of(context)
+                  .showSnackBar(const SnackBar(content: Text('This is a snackbar')));
+            },
           ),
+          IconButton(
+            icon: Icon(Icons.navigate_next),
+            onPressed: (){
+              Navigator.push(context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context){
+                        return Scaffold(
+                          appBar: AppBar(
+                            title: Text("next Page: 다음 페이집니다."),
+                          ),
+                          body: Center(
+                            child: Text("두번째 페이집니다...."),
+                          ),
+                        );
+                      }
+                  )
+              );
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+            "이것이 첫 페이지 입니다..."
         ),
       ),
     );
