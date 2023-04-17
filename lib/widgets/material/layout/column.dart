de5cf64beed377e55080b66324ff4a8bf385ce64
hyungeun.jin <kingwang09@gmail.com>
2023-04-17 10:11:53 +0900

    layout: column 기초

---
+++ lib/widgets/material/layout/column.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyColumnApp());
+}
+class MyColumnApp extends StatelessWidget{
+  const MyColumnApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyColumn(),
+    );
+  }
+
+}
+class MyColumn extends StatelessWidget{
+  const MyColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Column Demo...')
+      ),
+      body: Column(
+        children: const [
+          Text('그렇다면 유대인의 나은 점이 무엇이며 할례의 가치가 무엇입니까 ?'),
+          Text('여러 모로 많이 있습니다. 우선 그들은 하나님의 말씀을 맡았습니다. '),
+        ],
+      ),
+    );
+  }
+}
