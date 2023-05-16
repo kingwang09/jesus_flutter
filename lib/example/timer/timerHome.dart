ef4f9859c916b4495f9c190d17f539d6fd8a2093
hyungeun.jin <kingwang09@gmail.com>
2023-05-16 15:32:57 +0900

    비정상적으로 돌아는 가는데 아직 상태관리가 익숙치 않음.

--- lib/example/timer/timerHome.dart
+++ lib/example/timer/timerHome.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class TimerHome extends StatefulWidget{
@@ -10,6 +12,52 @@ class TimerHome extends StatefulWidget{
 }
 
 class _TimerHomeState extends State<TimerHome>{
+  DateTime? _start;
+  DateTime? _end;
+  String _display = "00:00";
+  bool _isRunning = false;
+
+  void start(){
+    _start = DateTime.now();
+    _isRunning = true;
+    runTimer();
+  }
+
+  void end(){
+    _end = DateTime.now();
+    _isRunning = false;
+    updateIsRunning(_isRunning);
+    updateDisplay(_end!);
+  }
+
+  void updateIsRunning(bool isRunning){
+    setState(() {
+      _isRunning = isRunning;
+    });
+  }
+
+  void updateDisplay(DateTime end){
+    int diffMinute = _start!.minute - end.minute;
+    int diffSeconds = _start!.second - end.second;
+    print("current: display=$diffMinute:$diffSeconds, isRunning=$_isRunning");
+
+    setState(() {
+      _display = "$diffMinute:$diffSeconds";
+    });
+  }
+
+  void runTimer() async{
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if(_isRunning) {
+        print("run second...");
+        updateDisplay(DateTime.now());
+      }else{
+        print("run close..");
+        timer.cancel();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +71,7 @@ class _TimerHomeState extends State<TimerHome>{
             height: MediaQuery.of(context).size.width * 0.5,
             child: Center(
               child: Text(
-                  "00:00",
+                  _display,
                   style: TextStyle(
                     fontSize: 48,
                     fontWeight: FontWeight.bold,
@@ -31,9 +79,17 @@ class _TimerHomeState extends State<TimerHome>{
               ),
             ),
           ),
-          ElevatedButton(onPressed: ()=>{},
-              child: Text("시작하기"),
+          Row(
+            children: [
+              ElevatedButton(onPressed: start,
+                child: Text("시작하기"),
+              ),
+              ElevatedButton(onPressed: end,
+                child: Text("종료하기"),
+              )
+            ],
           )
+
         ],
       ),
     );
